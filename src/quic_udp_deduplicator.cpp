c7fd5bdc36b4366015b64167638a1319ff4eeeb9
Edward <63866043+Telianedward@users.noreply.github.com>
2025-10-21 14:33:21 +0300

    fx
    Signed-off-by: Edward <63866043+Telianedward@users.noreply.github.com>

--- src/quic_udp_deduplicator.cpp
+++ src/quic_udp_deduplicator.cpp
@@ -3,31 +3,6 @@
 #include "../include/client_key.hpp"
 #include <cstring>
 
-// Реализация функций для структур, которые находятся в global scope, а не внутри Deduplicator
-
-size_t ClientKeyHash::operator()(const ClientKey &k) const noexcept
-{
-    std::hash<uint32_t> hasher;
-    size_t result = hasher(k.addr) ^
-                    (std::hash<uint16_t>()(k.port) << 1) ^
-                    std::hash<uint64_t>()(*reinterpret_cast<const uint64_t *>(k.cid));
-    return result;
-}
-
-bool ClientKeyEqual::operator()(const ClientKey &a, const ClientKey &b) const noexcept
-{
-    return a.addr == b.addr && a.port == b.port &&
-           std::memcmp(a.cid, b.cid, 8) == 0 &&
-           a.token == b.token; // Добавлено сравнение токена
-}
-
-bool ClientKey::operator==(const ClientKey &other) const noexcept
-{
-    return addr == other.addr && port == other.port &&
-           std::memcmp(cid, other.cid, 8) == 0 &&
-           token == other.token; // Добавлено сравнение токена
-}
-
 void Deduplicator::add_packet(const ClientKey &key, const PacketInfo &info)
 {
     packet_map_[key] = info;
