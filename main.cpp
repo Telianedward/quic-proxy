dbf85353c5b72b0ca2d044c9588e14bbe2a22bf2
Edward <63866043+Telianedward@users.noreply.github.com>
2025-11-01 23:29:50 +0300

    fx
    Signed-off-by: Edward <63866043+Telianedward@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 
 #include "include/http3/quic_udp_proxy.hpp"
 // #include "include/http2/server.hpp"
-#include "include/http1/server.hpp"
+#include "server.hpp"
 #include "logger.h"
 #include <thread>
 #include <iostream>
--- src/http1/server.cpp
+++ src/http1/server.cpp
@@ -12,7 +12,7 @@
  * @version 1.0
  * @license MIT
  */
-#include "../../include/http1/server.hpp"
+#include "server.hpp"
 #include <cstring>
 #include <algorithm>
 #include <sstream>
